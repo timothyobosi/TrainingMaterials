62a25a492448a0c596fae4734747501e410a14c1
timothyobosi <timothyobosi8@gmail.com>
2025-08-22 14:59:31 +0300

    extract Query params, No backend is available yet

---
+++ src/utils/parseQuery.js
@@ -0,0 +1,11 @@
+
+export const parseQuery =() =>{
+    const params = new URLSearchParams(window.location.search)
+    return{
+        audio1: params.get('audio1'),
+        audio2: params.get('audio2'),
+        audio3: params.get('audio4'),
+        audio: params.get('audio4'),
+        returnUrl:params.get('returnUrl'),
+    }
+}
