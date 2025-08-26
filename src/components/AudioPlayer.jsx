2583ae7b02595d3c8b9dbb9b14079e868c4f9cd9
timothyobosi <timothyobosi8@gmail.com>
2025-08-26 11:07:44 +0300

    time fix

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -71,7 +71,7 @@ const AudioPlayer = ({src, onEnded, onRestart,isPlaying, onPlayPause}) => {
   const formatTime = (time) => {
     const minutes = Math.floor(time/60);
     const seconds = Math.floor(time % 60);
-    return `${minutes}:{seconds < 10 ? '0' :''}${seconds}`;
+    return `${minutes}:${seconds < 10 ? '0' :''}${seconds}`;
   }
 
   return (
