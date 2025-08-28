4407db9377826f1a2f2487babd096c3ea0d24ef5
timothyobosi <timothyobosi8@gmail.com>
2025-08-28 16:41:16 +0300

    flot inpu

--- src/components/FloatingInput.jsx
+++ src/components/FloatingInput.jsx
@@ -1,19 +1,31 @@
 
 import { useState } from "react";
-import { FaEnvelope, FaLock } from "react-icons/fa";
+import { FaEnvelope, FaKey, FaLock } from "react-icons/fa";
 
 
 const iconMap = {
-    email: <FaEnvelope/>
-    password: <FaLock/>
+    email: <FaEnvelope/>,
+    password: <FaLock/>,
+    token: <FaKey/>
 };
 
 export default function Floating({label, type = "text",value, onChange,name}){
-    const [] = useState(false);
+    const [focused, setFocused] = useState(false);
 
     return(
-        <div>
-            
+        <div className="input-wrapper">
+            <div className="icon">{iconMap[name]} </div>
+            <input 
+            type={type} 
+            value={value}
+            name={name}
+            onChange={onChange}
+            onFocus={()=>setFocused(true)}
+            onBlur={() => setFocused(false)}
+            required
+            />
+            <label className={focused || value ? "float":""}>{label} </label>
+
         </div>
 
     );
